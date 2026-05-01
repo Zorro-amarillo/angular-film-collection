d2d1fdf911e827363a341d013fc5580411f6238d
Yana Malakhova <ya6ka0@gmail.com>
2026-05-01 03:52:31 +0300

    feat: add router-outlet and default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Catalog } from './pages/catalog/catalog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Catalog,
+  },
+];
