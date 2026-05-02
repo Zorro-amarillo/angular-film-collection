2648a052ac142bd799bdd30ee4bfaa8c73676f73
Yana Malakhova <ya6ka0@gmail.com>
2026-05-02 05:29:23 +0300

    feat: handle unknown routes with 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Catalog } from './pages/catalog/catalog';
 import { FilmDetails } from './pages/film-details/film-details';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'films/:filmId',
     component: FilmDetails,
+  },
+  {
+    path: '**',
+    component: NotFound,
   },
 ];
