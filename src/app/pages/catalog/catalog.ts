6286ac7e88f047284b930cbba901bdb84b4ef63d
Yana Malakhova <ya6ka0@gmail.com>
2026-05-05 02:28:53 +0300

    feat: add search functionality with signals

--- src/app/pages/catalog/catalog.ts
+++ src/app/pages/catalog/catalog.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
 
 import { FilmCard } from '../../components/film-card/film-card';
 import { FilmService } from '../../services/film.service';
@@ -17,7 +17,23 @@ export class Catalog implements OnInit {
 
   films = this.filmService.getFilms();
 
+  searchInput = signal<string>('');
+  foundFilms = computed(() => {
+    const textInput = this.searchInput().toLowerCase().trim();
+
+    if (!textInput) {
+      return this.films();
+    }
+
+    return this.films().filter((film) => film.title.toLowerCase().includes(textInput));
+  });
+
   ngOnInit(): void {
     this.breadcrumbsService.setPage('');
+  }
+
+  onTextInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.searchInput.set(input.value);
   }
 }
