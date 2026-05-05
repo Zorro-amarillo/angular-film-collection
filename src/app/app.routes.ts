726303deb699f9ab4d0aa13baf8ab08df013795e
Yana Malakhova <ya6ka0@gmail.com>
2026-05-05 22:49:13 +0300

    feat: add about-page with routing and breadcrumbs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+
 import { Catalog } from './pages/catalog/catalog';
 import { FilmDetails } from './pages/film-details/film-details';
 import { NotFound } from './pages/not-found/not-found';
+import { About } from './pages/about/about';
 
 export const routes: Routes = [
   {
@@ -11,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'films/:filmId',
     component: FilmDetails,
+  },
+  {
+    path: 'about',
+    component: About,
   },
   {
     path: '**',
